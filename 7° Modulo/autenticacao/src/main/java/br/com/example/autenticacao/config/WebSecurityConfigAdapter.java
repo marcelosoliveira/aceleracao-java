c60549f3f6be539d4efa63943d8522cbe86c125f
marcelosoliveira <msbobsk8@gmail.com>
2021-04-04 21:26:15 -0300

    Testes spring boot security

---
+++ 7° Modulo/autenticacao/src/main/java/br/com/example/autenticacao/config/WebSecurityConfigAdapter.java
@@ -0,0 +1,43 @@
+package br.com.example.autenticacao.config;
+
+import br.com.example.autenticacao.dto.UsuarioCustomDTO;
+import br.com.example.autenticacao.model.Usuario;
+import br.com.example.autenticacao.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfigAdapter extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public AuthenticationManager customAuthenticationManager() throws Exception {
+        return authenticationManagerBean();
+    }
+
+    @Autowired
+    public void authenticationManager(AuthenticationManagerBuilder builder, UsuarioRepository usuarioRepository)
+            throws Exception {
+        if (usuarioRepository.count() == 0) {
+            Usuario usuario = new Usuario();
+            usuario.setLogin("admin");
+            usuario.setSenha(passwordEncoder().encode("admin"));
+            usuarioRepository.save(usuario);
+        }
+
+        builder.userDetailsService(login -> new UsuarioCustomDTO(usuarioRepository.findByLogin(login)))
+                .passwordEncoder(passwordEncoder());
+    }
+
+    @Bean
+    public static BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
